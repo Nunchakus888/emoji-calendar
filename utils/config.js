aad6c4f80932278d4c6120b95fbb04df792ab38c
23333333 <1023268613@qq.com>
2024-07-09 13:11:24 +0000

    ....

---
+++ utils/config.js
@@ -0,0 +1,5 @@
+
+export const metadata = {
+    title: "Printable Calendars",
+    description: `Printing a calendar is as simple as clicking a button. Just hit the big red \"Print\" button and you have your calendar. Want to add notes? \n Click on a day and start typing. No internet connection? No problem. Download one of our many available PDF calendars and print directly from your computer.`,
+};
