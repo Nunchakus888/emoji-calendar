9c65346ca36a4d56bec444ab8342d911fe71eaf5
23333333 <1023268613@qq.com>
2024-08-07 09:13:15 +0000

    remove the description of footer

---
+++ components/ui/common.tsx
@@ -0,0 +1,27 @@
+import { getLocale } from "@/utils";
+
+export const FooterDscription = async ({ locale }) => {
+    const lang = getLocale(locale);
+    const { description } = await import(`@/services/i18n/${lang}.json`).catch(() => import(`@/services/i18n/0.json`));
+  
+    return (
+      <div className="flex w-full items-center flex-col gap-2">
+        <div className="text-center hidden md:block">
+          {
+            description.split("\n").map((line, index) => (
+              <p key={index} className={"mb-1"}>
+                {line}
+              </p>
+            ))
+          }
+        </div>
+  
+        <div className="flex items-center text-gray-300 text-xs w-full justify-center pb-2 gap-2">
+            © 2024 x-calendar. All rights reserved.
+            <a href="https://x.com/X_CalendarPrint">
+              Contact Us
+            </a>
+          </div>
+      </div>
+    );
+  };
