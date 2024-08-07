fdd8ae741fa9a1e0541acc012cf931b5b9a012f7
23333333 <1023268613@qq.com>
2024-08-07 13:12:46 +0000

    layout: optimized

---
+++ components/ui/server.tsx
@@ -0,0 +1,10 @@
+export const CopyRight = () => {
+    return (
+      <div className="flex items-center text-gray-300 text-xs w-full justify-center pb-2 gap-2 print:hidden">
+        © 2024 x-calendar. All rights reserved.
+        <a href="https://x.com/X_CalendarPrint">
+          Contact Us
+        </a>
+      </div>
+    )
+  }
