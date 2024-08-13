2648d92d8893aa32ff176792a43190c52dee534f
23333333 <1023268613@qq.com>
2024-08-13 04:48:42 +0000

    SEO: added description content to body with h1 tag

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -6,7 +6,7 @@ export default async function Page({ params }) {
   const { description } = await dynamicMetadata(params);
   return (
     <>
-      <h1 className="absolute top-0 w-0 invisible">{description}</h1>
+      <h1 className="absolute top-0 w-0 h-0 invisible">{description}</h1>
       <EmojiCalendar lang={params.lang || "en-US"} />
     </>
   )
