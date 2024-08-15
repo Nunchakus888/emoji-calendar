673c7d5bfcf13765c13f225094f28354e2327daa
23333333 <1023268613@qq.com>
2024-08-15 08:06:21 +0000

    SEO: added h1 tag to the body top

--- app/[lang]/[...date]/page.tsx
+++ app/[lang]/[...date]/page.tsx
@@ -1,5 +1,12 @@
 import EmojiCalendar from "@/components/calendar";
+import { dynamicMetadata } from "@/utils/config";
 
 export default async function Page({ params }) {
-  return <EmojiCalendar lang={params.lang || "en-US"} />;
+  const { description } = await dynamicMetadata(params);
+  return (
+    <>
+      <h1 className="absolute top-0 w-0 h-0 invisible">{description}</h1>
+      <EmojiCalendar lang={params.lang || "en-US"} />
+    </>
+  )
 }
